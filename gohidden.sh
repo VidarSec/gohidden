9037dc7718f6b42c548b05a15c556298d4e7a3e1
VidarDev <vidar@24x7.gg>
2022-10-23 01:04:27 +0200

    aa

--- gohidden.sh
+++ gohidden.sh
@@ -4,6 +4,13 @@ set -eu
 
 # status indicators
     greenplus='\e[1;33m[++]\e[0m'
+    greenminus='\e[1;33m[--]\e[0m'
+
+# silent mode
+    silent=''                  # uncomment to see all output
+    # silent='>/dev/null 2>&1' # uncomment to hide all output10
+    export DEBIAN_FRONTEND=noninteractive
+    export PYTHONWARNINGS=ignore
 
 apt_update() {
     echo -e "\n  $greenplus running: apt update \n"
@@ -30,6 +37,16 @@ apt_upgrade_complete() {
 
 apt_autoremove_complete() {
     echo -e "\n  $greenplus apt autoremove - complete"
+    }
+
+install_nginx () {
+    if [[ -f /usr/bin/nginx ]]; then
+      echo -e "\n  $greenminus  nginx already installed - skipping"
+    else
+    	echo -e "\n  $greenplus installing nginx"
+      apt_update && apt_update_complete && apt -y install nginx
+      echo -e "\n  $greenplus  nginx - installed "
+    fi
     }
 
 apt_update
